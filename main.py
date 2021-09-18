4aa2cee53f1faf8cf94adc7237f585be45456b30
Gennalynb123 <gennalyn@live.com>
2021-09-17 17:16:31 -0700

    Fixed Greet Code and Changes to Binary lab

--- main.py
+++ main.py
@@ -11,25 +11,52 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/greet', methods=['GET', 'POST'])
-def greet():
-    # submit button has been pushed
-    if request.form:
-        name = request.form.get("name")
-        if len(name) != 0:  # input field has content
-            return render_template("stub.html", name=name)
-    # starting and empty input default
-    return render_template("stub.html", name="World")
+@app.route('/Test/')
+def Test():
+    return render_template("Test.html")
 
-@app.route('/ALI', methods=['GET', 'POST'])
-def ALI():
-    # submit button has been pushed
-    if request.form:
-        name = request.form.get("name")
-        if len(name) != 0:  # input field has content
-            return render_template("Ali.html", name=name)
-    # starting and empty input default
-    return render_template("Ali.html", name="World")
+
+@app.route('/Results/')
+def Results():
+    return render_template("Results.html")
+
+
+
+@app.route('/Gennalyn/')
+def Gennalyn():
+    return render_template("Gennalyn.html")
+
+
+@app.route('/Rithwikh/')
+def Rithwikh():
+    return render_template("Rithwikh.html")
+
+
+@app.route('/Jun/')
+def Jun():
+    return render_template("Jun.html")
+
+
+@app.route('/Lucas/')
+def Lucas():
+    return render_template("Lucas.html")
+
+
+@app.route('/Ali/')
+def Ali():
+    return render_template("Ali.html")
+
+
+@app.route('/Bimary/')
+def Bimary():
+    return render_template("Bimary.html")
+
+
+@app.route('/Home/')
+def Home():
+    return render_template("Home.html")
+
+
 
 @app.route('/GENNALYN', methods=['GET', 'POST'])
 def GENNALYN():
@@ -41,6 +68,7 @@ def GENNALYN():
     # starting and empty input default
     return render_template("Gennalyn.html", name="World")
 
+
 @app.route('/JUN', methods=['GET', 'POST'])
 def JUN():
     # submit button has been pushed
@@ -50,6 +78,7 @@ def JUN():
             return render_template("Jun.html", name=name)
     # starting and empty input default
     return render_template("Jun.html", name="World")
+
 
 @app.route('/LUCAS', methods=['GET', 'POST'])
 def LUCAS():
@@ -61,6 +90,7 @@ def LUCAS():
     # starting and empty input default
     return render_template("Lucas.html", name="World")
 
+
 @app.route('/RITHWIKH', methods=['GET', 'POST'])
 def RITHWIKH():
     # submit button has been pushed
@@ -71,89 +101,35 @@ def RITHWIKH():
     # starting and empty input default
     return render_template("Rithwikh.html", name="World")
 
-@app.route('/BIMARY', methods=['GET', 'POST'])
-def BIMARY():
+
+@app.route('/ALI', methods=['GET', 'POST'])
+def ALI():
     # submit button has been pushed
     if request.form:
         name = request.form.get("name")
         if len(name) != 0:  # input field has content
-            return render_template("Bimary.html", name=name)
+            return render_template("Ali.html", name=name)
     # starting and empty input default
-    return render_template("Bimary.html", name="World")
-
-# connects /kangaroos path to render kangaroos.html
-@app.route('/kangaroos/')
-def kangaroos():
-    return render_template("kangaroos.html")
+    return render_template("Ali.html", name="World")
 
 
-@app.route('/walruses/')
-def walruses():
-    return render_template("walruses.html")
-
-@app.route('/home/')
-def home():
-    return render_template("Home.html")
-
-@app.route('/hawkers/')
-def hawkers():
-    return render_template("hawkers.html")
-
-@app.route('/Bimary/')
-def Bimary():
-    return render_template("Bimary.html")
-
-@app.route('/rgb/')
-def rgb():
-    return render_template("rgb.html")
+@app.route('/myFunction()', methods=['GET', 'POST'])
+def myFunction():
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("text")
+        if len(name) != 0:  # input field has content
+            return render_template("Bimary.html", name=text1)
+    # starting and empty input default
+    return render_template("Bimary.html", name="text1")
 
 
-@app.route('/stub/')
-def stub():
-    return render_template("stub.html")
 
-@app.route('/Rithwikh/')
-def Rithwikh():
-    return render_template("Rithwikh.html")
 
-@app.route('/Gennalyn/')
-def Gennalyn():
-    return render_template("Gennalyn.html")
 
-@app.route('/Ali/')
-def Ali():
-    return render_template("Ali.html")
 
-@app.route('/Jun/')
-def Jun():
-    return render_template("Jun.html")
-
-@app.route('/Lucas/')
-def Lucas():
-    return render_template("Lucas.html")
-
-@app.route('/Binary/')
-def Binary():
-    return render_template("Binary.html")
-
-@app.route('/Results/')
-def Results():
-    return render_template("Results.html")
-
-@app.route('/Test/')
-def Test():
-    return render_template("Test.html")
 
 # runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True)
 
-@app.route('/BINARY', methods=['GET', 'POST'])
-def BINARY():
-    # submit button has been pushed
-    if request.form:
-        name = request.form.get("name")
-        if len(name) != 0:  # input field has content
-            return render_template("Binary.html", name=name)
-    # starting and empty input default
-    return render_template("Binary.html", name="World")
